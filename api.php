b8168e3b4d7f3c63c67496d378dfe36d1cae7c2a
alireza nikju <alirezanikjoo2018@gmail.com>
2025-06-27 17:00:46 +0330

    creat api and send data with get method to api.php and get information of api.php

--- api.php
+++ api.php
@@ -1,11 +1,28 @@
 <?php
 
+// header("Constant-Type: application/json");
+
+// $response = ["status" => "ok" ,"msg" => "hello my friend"];
+
+
+
+// echo json_encode($response);
+
+
 header("Constant-Type: application/json");
 
-$response = ["status" => "ok" ,"msg" => "hello my friend"];
+if(isset($_GET['name']) && isset($_GET['job'])){
 
+    $name = $_GET['name'];
+    $job = $_GET['job'];
 
+    $response = ["status"=>"ok","msg"=>"اطلاعات دریافت شد","data"=>["name"=>"$name
+    ", "job"=>"$job"]];
 
-echo json_encode($response);
+}else{
+    $response = ["status"=>"error","msg"=>"مقدار های کوفتی رو پر کن","data"=>["name"=>"خالی
+    ", "job"=>"خالی"]];
+}
 
+echo json_encode($response , JSON_UNESCAPED_UNICODE);
 ?>
--- testCurl.php
+++ testCurl.php
@@ -1,14 +1,17 @@
 <?php
 
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL,"http://localhost/Api/api.php");
+$url = "http://localhost/Api/api.php?name=alireza&job=teacher";
+curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
 $response = curl_exec($ch);
 curl_close($ch);
 
 $data = json_decode($response , true);
 
-echo $data["status"];
-echo "<br>";
-echo $data["msg"];
+echo $data["status"]."<br>";
+echo $data["msg"]."<br>";
+echo "name: ".$data["data"]["name"]."<br>";
+echo "job: ".$data["data"]["job"];
+
+
